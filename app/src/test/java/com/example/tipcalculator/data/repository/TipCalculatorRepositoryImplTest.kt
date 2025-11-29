42aa6909f10d16177e08badc5f74e4136b7ade6f
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2025-11-29 18:55:07 -0500

    0 percent and 100 percent tip unit test

--- app/src/test/java/com/example/tipcalculator/data/repository/TipCalculatorRepositoryImplTest.kt
+++ app/src/test/java/com/example/tipcalculator/data/repository/TipCalculatorRepositoryImplTest.kt
@@ -2,6 +2,7 @@ package com.example.tipcalculator.data.repository
 
 import com.example.tipcalculator.data.models.Tip
 import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TipCalculatorRepositoryImplTest{
@@ -18,6 +19,33 @@ class TipCalculatorRepositoryImplTest{
 
         // Then
         assertThat(calculatedTip).isEqualTo(15.0)
+    }
 
+    @Test
+    fun `check 0 percent tip is calculated correctly`(){
+        // Given
+        val percent = 0f
+        val billAmount = 100.0
+        val tip = Tip(0, percent)
+
+        // When
+        val calculatedTip = TipCalculatorRepositoryImpl().calculateTip(tip, billAmount)
+
+        // Then
+        assertThat(calculatedTip).isEqualTo(0.0)
+    }
+
+    @Test
+    fun `check 100 percent tip is calculated correctly`(){
+        // Given
+        val percent = 100f
+        val billAmount = 100.0
+        val tip = Tip(0, percent)
+
+        // When
+        val calculatedTip = TipCalculatorRepositoryImpl().calculateTip(tip, billAmount)
+
+        // Then
+        assertThat(calculatedTip).isEqualTo(100.0)
     }
 }
