ee6f1c32c3c56817f7d507168461455c31da1d5a
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2026-01-02 14:42:06 -0500

    printing ui nodes

--- app/src/androidTest/java/com/example/tipcalculator/ui/home/HomeScreenKtTest.kt
+++ app/src/androidTest/java/com/example/tipcalculator/ui/home/HomeScreenKtTest.kt
@@ -1,22 +1,25 @@
 package com.example.tipcalculator.ui.home
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.tipcalculator.data.repository.TipCalculatorRepository
 import com.example.tipcalculator.data.repository.TipCalculatorRepositoryImpl
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class HomeScreenKtTest{
 
     private lateinit var repository:TipCalculatorRepository
     private lateinit var homeViewModel: HomeViewModel
 
-    @get:Rule
+    @get:Rule // Applies before and after every test
     val composeTestRule = createComposeRule()
 
-    @Before
+    @Before // Runs before each test
     fun setup(){
         repository = TipCalculatorRepositoryImpl()
         homeViewModel = HomeViewModel(repository)
@@ -25,5 +28,12 @@ class HomeScreenKtTest{
                 HomeScreen(homeViewModel = homeViewModel)
             }
         }
+    }
+
+
+
+    @Test
+    fun app_launches(){
+        composeTestRule.onRoot().printToLog(tag = "homescreen")
     }
 }
